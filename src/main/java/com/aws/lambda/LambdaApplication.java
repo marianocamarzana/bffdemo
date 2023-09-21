13398dd168853b5f9fc2b747a38ee8478898ec2e
Mariano Camarzana <mariano.camarzana@consultores.wom.cl>
2023-09-21 20:43:28 -0300

    add swagger configuration

--- src/main/java/com/aws/lambda/LambdaApplication.java
+++ src/main/java/com/aws/lambda/LambdaApplication.java
@@ -1,9 +1,16 @@
 package com.aws.lambda;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Spring webflux BFF example",
+		version = "1.0",
+		description = "sample documents"
+))
 public class LambdaApplication {
 
 	public static void main(String[] args) {
